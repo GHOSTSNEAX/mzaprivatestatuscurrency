e8927838aaa7086746f5467a29eb1f995f4d5336
GHOSTSNEAX <ethanmhunt91@gmail.com>
2025-03-12 14:32:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["⛔️ Working in MZA", "🎨 Being more creative"];
+const statusMessages = ["⛔️ Working in MZA", "🎨 Making World More Creative"];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
